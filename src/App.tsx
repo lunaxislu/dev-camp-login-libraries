f2faa4113085872ae53292ce717f1da99f6a5a60
lunaxislu <cmk0905@naver.com>
2024-03-04 23:55:35 +0900

    🎉 accordian

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
 import { useState } from "react";
 import "./App.css";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 function App() {
-  return <>asdf</>;
+  return (
+    <>
+      <Accordion type="single" collapsible className={"w-full"}>
+        <AccordionItem value="item-1">
+          <AccordionTrigger className="">Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </>
+  );
 }
 
 export default App;
